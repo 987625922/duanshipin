11fe26b0ce3fb03c7dc427260ba97ffa007606d6
98762 <987625922@qq.com>
2020-08-21 00:29:14 +0800

    更新login.html

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,9 @@
+function inputGetFocuse(){
+    var elemnet = document.getElementsByName("div-login-form-u1");
+    elemnet.setAttribute("border-style","solid");
+    elemnet.setAttribute("border-color","#44c9a8");
+    elemnet.setAttribute("border-width","1px");
+}
+function inputLoseFocuse(){
+    console.log("22")
+}

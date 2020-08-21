63f49b625209e98a00e557355a19bb9ec82ffc6e
wind <987625922@qq.com>
2020-08-21 18:48:27 +0800

    更新

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,9 +1,18 @@
+// 账号密码框获取到焦点
 function inputGetFocuse(){
-    var elemnet = document.getElementsByName("div-login-form-u1");
-    elemnet.setAttribute("border-style","solid");
-    elemnet.setAttribute("border-color","#44c9a8");
-    elemnet.setAttribute("border-width","1px");
+    document.getElementById("div-login-form-u1").style.border = 'solid 1px #44c9a8'
 }
+// 账号密码框失去了焦点
 function inputLoseFocuse(){
-    console.log("22")
+    document.getElementById("div-login-form-u1").style.border = 'solid 0px #44c9a8'
+}
+// 验证码框获取到焦点
+function vcodeGetFocuse(){
+    document.getElementById("input-form-login-vcode").style.border = 'solid 1px #999'
+}
+function vcodeLoseFocuse(){
+    document.getElementById("input-form-login-vcode").style.border = 'solid 1px #d9d9d9'
+}
+function login(){
+    document.getElementById('div-login-form-u4').style.display = 'block'
 }

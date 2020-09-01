563fab9c70ba8046ace10566ac37f1598e930508
wind <987625922@qq.com>
2020-09-01 18:05:14 +0800

    update

---
+++ src/main/resources/static/js/albumUserVideo.js
@@ -0,0 +1,91 @@
+// 把页面所在左边的item设置为选中
+document.getElementById('left_item_one').style.backgroundColor = '#191d26';
+document.getElementById('left_item_one_a').style.color = '#fff';
+document.getElementById('left_item_one_img').style.backgroundImage = "url('../../../static/img/video_content_h.png')";
+document.getElementById('item_one_one').style.borderLeft = '3px solid #44c9a8';
+var videoPoolDom = document.getElementById('operate_video_pool');
+videoPoolDom.style.backgroundColor = '#fff'
+videoPoolDom.style.color = '#333'
+videoPoolDom.style.border = '1px solid #dcdfe6'
+var userVideoPoolDom = document.getElementById('user_video_pool');
+userVideoPoolDom.style.backgroundColor = '#44c9a8';
+userVideoPoolDom.style.color = '#fff';
+userVideoPoolDom.style.borderWidth = '0px';
+//页码选择器
+var pageSelect = document.getElementById('page_select');
+pageSelect.style.display = 'none'
+// 一进入界面显示上线的按钮的controller
+document.getElementById('controll_online').style.display = 'block'
+// document.getElementById('table_controll_one').style.display = 'block'
+// 上线的点击事件
+var btnOnline = document.getElementById('online');
+var btnOffline = document.getElementById('offline');
+var btnDel = document.getElementById('del');
+var btnRecommend = document.getElementById('recommend');
+// 各个按钮显示的controller
+var onlineControll = document.getElementById('controll_online');
+var offlineControll = document.getElementById('controll_offline');
+var delControll = document.getElementById('controll_del');
+var recommendContrll = document.getElementById('controll_recommend');
+// 回收站table里的控制和其他按钮不一样，要单独分开处理
+var tableControllerOne = document.getElementById('table_controll_one');
+var tableControllerTwo = document.getElementById('table_controll_two');
+// 正在选中的按钮
+var isSelectBtn = btnOnline
+btnOnline.onclick = function () {
+    btnPre(btnOnline);
+    btnCommon(isSelectBtn);
+    isSelectBtn = btnOnline;
+    onlineControll.style.display = 'block'
+    offlineControll.style.display = 'none'
+    delControll.style.display = 'none'
+    recommendContrll.style.display = 'none'
+    // tableControllerOne.style.display = 'block'
+    // tableControllerTwo.style.display = 'none'
+}
+//草稿的点击事件
+btnOffline.onclick = function () {
+    btnPre(btnOffline);
+    btnCommon(isSelectBtn);
+    isSelectBtn = btnOffline;
+    offlineControll.style.display = 'block'
+    onlineControll.style.display = 'none'
+    delControll.style.display = 'none'
+    recommendContrll.style.display = 'none'
+    // tableControllerOne.style.display = 'block'
+    // tableControllerTwo.style.display = 'none'
+}
+btnDel.onclick = function () {
+    btnPre(btnDel);
+    btnCommon(isSelectBtn);
+    isSelectBtn = btnDel;
+    offlineControll.style.display = 'none'
+    onlineControll.style.display = 'none'
+    delControll.style.display = 'block'
+    recommendContrll.style.display = 'none'
+    // tableControllerOne.style.display = 'none'
+    // tableControllerTwo.style.display = 'block'
+}
+btnRecommend.onclick = function () {
+    btnPre(btnRecommend);
+    btnCommon(isSelectBtn);
+    isSelectBtn = btnRecommend;
+    offlineControll.style.display = 'none'
+    onlineControll.style.display = 'none'
+    delControll.style.display = 'none'
+    recommendContrll.style.display = 'block'
+    // tableControllerOne.style.display = 'block'
+    // tableControllerTwo.style.display = 'none'
+}
+//把view改成按下按钮的颜色
+function btnPre(dom) {
+    dom.style.borderWidth = '0px';
+    dom.style.backgroundColor = '#44c9a8'
+    dom.style.color = '#fff'
+}
+//把view改成普通颜色
+function btnCommon(dom) {
+    dom.style.border = '1px solid #dcdfe6';
+    dom.style.backgroundColor = '#fff'
+    dom.style.color = '#333'
+}
--- src/main/resources/static/js/commonLayout.js
+++ src/main/resources/static/js/commonLayout.js
@@ -1,14 +1,15 @@
 // 输出头代码
 function writeHander() {
     document.writeln("<div class=\'hander\'>");
-    // document.writeln("        <div class=\'inner_c\'>");
     document.writeln("            <div class=\'logo\'>");
     document.writeln("                    <a href=\'#\'>");
     document.writeln("                <img src=\'../../../static/img/logo-white.png\' alt=\'\' class=\'logo_img\'/>");
     document.writeln("                    </a>");
     document.writeln("            </div>");
+    document.writeln("                    <a href=\'#\'>");
     document.writeln("                <img src=\'../../../static/img/tx.jpg\' alt=\'\' class=\'hander_per_icon\'/>");
     document.writeln("                <span class=\'hander_per_name\'>吴彦祖</span>");
+    document.writeln("                    </a>");
     document.writeln("            <div class=\'account_controll\'>");
     document.writeln("            <span>");
     document.writeln("                    <a class=\'account\' href=\'#\'>");
@@ -19,7 +20,6 @@ function writeHander() {
     document.writeln("                    </a>");
     document.writeln("            </span>");
     document.writeln("            </div>");
-    // document.writeln("        </div>");
     document.writeln("    </div>");
 }
 // 输出左边栏
@@ -27,52 +27,46 @@ function writeLeft() {
     document.writeln("<div class=\'left\'>");
     document.writeln("                    <div class=\'left_nav\'>");
     document.writeln("                        <div class=\'left_item left_item_one\' id=\'left_item_one\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/video_content_n.png\' id=\'left_item_one_img\' alt=\'\'>");
     document.writeln("                            <a href=\'#\' id=\'left_item_one_a\'>");
-    document.writeln("                                内容");
+    document.writeln("                                <i id=\'left_item_one_img\'>");
+    document.writeln("                                 </i>");
+    document.writeln("                                <span>内容</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                        <div class=\'left_item left_item_two\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/per_n.png\' alt=\'\'>");
     document.writeln("                            <a href=\'#\'>");
-    document.writeln("                                用户");
+    document.writeln("                                <i id=\'left_item_two_img\'>");
+    document.writeln("                                 </i>");
+    document.writeln("                                <span>用户</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                        <div class=\'left_item left_item_three\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/ev_n.png\' alt=\'\'>");
     document.writeln("                            <a href=\'#\'>");
-    document.writeln("                                审核");
+    document.writeln("                                <i id=\'left_item_three_img\'>");
+    document.writeln("                                </i>");
+    document.writeln("                                <span>审核</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                        <div class=\'left_item left_item_four\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/app_n.png\' alt=\'\'>");
     document.writeln("                            <a href=\'#\'>");
-    document.writeln("                                app");
+    document.writeln("                                <i id=\'left_item_four_img\'>");
+    document.writeln("                                </i>");
+    document.writeln("                                <span>app</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                        <div class=\'left_item left_item_five\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/msg_n.png\' alt=\'\'>");
     document.writeln("                            <a href=\'#\'>");
-    document.writeln("                                消息客服");
+    document.writeln("                                <i id=\'left_item_five_img\'>");
+    document.writeln("                                </i>");
+    document.writeln("                                <span>消息客服</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                        <div class=\'left_item left_item_six\'>");
-    document.writeln("                        <div>")
-    document.writeln("                                <img src=\'../../../static/img/sys_n.png\' alt=\'\'>");
     document.writeln("                            <a href=\'#\'>");
-    document.writeln("                                系统");
+    document.writeln("                                <i id=\'left_item_six_img\'>");
+    document.writeln("                                </i>");
+    document.writeln("                                <span>系统</span>");
     document.writeln("                            </a>");
-    document.writeln("                        </div>");
     document.writeln("                        </div>");
     document.writeln("                    </div>");
     document.writeln("                </div>");
@@ -80,21 +74,21 @@ function writeLeft() {
 function writeLeftTwoOfLeft() {
     document.writeln("<div class=\'left_two\'>");
     document.writeln("                    <div class=\'left_two_nav\'>");
-    document.writeln("                        <div id=\'item\'>");
+    document.writeln("                        <div id=\'left_two_nav_item\'>");
     document.writeln("                            <span>视频管理</span>");
     document.writeln("                        </div>");
-    document.writeln("                        <div class=\'item_one_one item_style\' id=\'item_one_one\'>");
+    document.writeln("                        <a  href=\"#\" class=\'item_one_one item_style\' id=\'item_one_one\'>");
     document.writeln("                            <span>专辑管理</span>");
-    document.writeln("                        </div>");
-    document.writeln("                        <div class=\'item_one_two item_style\'>");
+    document.writeln("                        </a>");
+    document.writeln("                        <a  href=\"#\" class=\'item_one_two item_style\'>");
     document.writeln("                            <span>影视管理</span>");
-    document.writeln("                        </div>");
-    document.writeln("                        <div id=\'item\'>");
+    document.writeln("                        </a>");
+    document.writeln("                        <div id=\'left_two_nav_item\'>");
     document.writeln("                            <span>标签管理</span>");
     document.writeln("                        </div>");
-    document.writeln("                        <div class=\'item_two_one item_style\'>");
+    document.writeln("                        <a href=\"#\" class=\'item_two_one item_style\'>");
     document.writeln("                            <span>标签管理</span>");
-    document.writeln("                        </div>");
+    document.writeln("                        </a>");
     document.writeln("                    </div>");
     document.writeln("                </div>");
 }

4d1add565231c0e8f9539a3217fff1f4d058c385
98762 <987625922@qq.com>
2020-09-18 21:51:30 +0800

    web框架换成threamleaf

--- duanshipin-dao/src/main/java/com/ljf/duanshipin/mapper/AdminMapper.java
+++ duanshipin-dao/src/main/java/com/ljf/duanshipin/mapper/AdminMapper.java
@@ -18,7 +18,6 @@ public interface AdminMapper {
             ",role_id,last_control_admin_id,password) VALUES(#{userName},#{realName},#{phone},#{status},#{createTime}" +
             ",#{lastUpdateTime},#{roleId},#{lastControlAdminId},#{password})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
-//获取返回的主键id
     int insert(Admin admin);
 
 
